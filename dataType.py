99252f0511d9dc2b7ed40d7c0f3b7fb2747a1740
Daksh Chuchra <chuchra.daksh4@gmail.com>
2025-08-08 20:55:54 +0530

    data types

---
+++ dataType.py
@@ -0,0 +1,6 @@
+arr = []
+arr.append(9988)
+arr.append(2191)
+arr.append(61)
+arr.append("it's daksh number")
+print(arr)
--- input.py
+++
@@ -1,4 +0,0 @@
-daksh = input("what is your name? ")
-print("Hello, " + daksh + "!")
-input("Press Enter to exit...")
-print (f'GoodBYE {daksh}! Have a nice day!')
