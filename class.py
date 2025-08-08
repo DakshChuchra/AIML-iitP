83c5b40e00fb37036d8d3f039c1d57122da0f8b9
Daksh Chuchra <chuchra.daksh4@gmail.com>
2025-08-08 20:25:28 +0530

    concating

---
+++ class.py
@@ -0,0 +1,5 @@
+name = "daksh"
+age = 20
+print(f"Hello, {name}! You are {age} years old.")
+print("Hello, " + name + "!" + " You are " + str(age) + " years old.")
+print(f"Press Enter to exit... {name}!")
