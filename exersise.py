8a9b34931195053e4ca303c20595b46e9e18d077
Daksh Chuchra <chuchra.daksh4@gmail.com>
2025-08-08 22:13:36 +0530

    todays exersice

--- exersise.py
+++ exersise.py
@@ -1,2 +1,9 @@
 user = int(input("your favorate number is : "))
 print("your favorate dubble number is : " + str(user * 2) )
+
+hobbies = ["reading", "coding", "gaming"]
+
+for hobby in hobbies:
+    print(f"I enjoy {hobby} in my free time.")
+
+
--- hobbies.py
+++
@@ -1,6 +0,0 @@
-hobbies = ["reading", "coding", "gaming"]
-
-for hobby in hobbies:
-    print(f"I enjoy {hobby} in my free time.")
-
-
