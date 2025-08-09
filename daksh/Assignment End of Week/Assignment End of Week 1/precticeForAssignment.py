2c16d9a8284699c1daa5cbc3acf50927de0d80b5
Daksh Chuchra <chuchra.daksh4@gmail.com>
2025-08-09 16:55:32 +0530

    Add initial script outline for assignment exercises

---
+++ daksh/Assignment End of Week/Assignment End of Week 1/precticeForAssignment.py
@@ -0,0 +1,12 @@
+# 1. Write a script to count the number of words in a given string.
+# 2. Take a sentence from the user and print:
+# Total words
+# Total characters (excluding spaces)
+# 3. Write a program to simulate a simple calculator (add, subtract, multiply, divide).
+# 4. Take a comma-separated list of integers from input, then print:
+# Sorted list
+# Sum of numbers
+# 5. Write a script that reads a text file and counts how many times the word “Python” appears.
+# 6. Write a program to generate a multiplication table for a given number up to 10.
+# 7. Create a script that prints the current working directory using os module.
+# 8. Write a program to create and activate a virtual environment using Python code
