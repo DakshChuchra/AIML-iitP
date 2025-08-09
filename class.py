6e7368df71579ce513dacc304ca4ccf8e735ee51
Daksh Chuchra <chuchra.daksh4@gmail.com>
2025-08-09 15:44:31 +0530

    class.py

--- class.py
+++ class.py
@@ -3,3 +3,24 @@ age = 20
 print(f"Hello, {name}! You are {age} years old.")
 print("Hello, " + name + "!" + " You are " + str(age) + " years old.")
 print(f"Press Enter to exit... {name}!")
+
+arr = []
+arr.append(9988)
+arr.append(2191)
+arr.append(61)
+arr.append(" it's daksh number")
+print(arr)
+
+num = ''
+for i in arr:
+    num += str(i)
+print(num)
+
+
+marks = {}
+
+marks["daksh"] = 100
+marks["sachin"] = 99
+marks["sachin"] = 98.5
+
+print(marks)
